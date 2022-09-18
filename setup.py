abc437a09f4a57db94e3e8e23ad9d28b0fc24a12
TheFungusAmongUs <nathan.pfeffer@protonmail.com>
2022-09-18 01:39:37 -0400

    Add function to set up config file from base

---
+++ setup.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from utils.file_handling import *
+
+
+def setup_toml() -> None:
+    if Path('config/config.toml').exists():
+        with open("config/config.toml", "r+") as file, open("config/base_config.toml", "r") as base_file:
+            # Adds any extra fields from the base config file when updating the bot
+            base_toml = base_file.read()
+            current_toml = base_file.read()
+            write_toml({**base_toml, **current_toml}, file)
+
+    else:
+        with open("config/config.toml", "w") as file, open("config/base_config.toml", "r") as base_file:
+            write_toml(toml.load(base_file), file)
+
+
+if __name__ == "__main__":
+    setup_toml()
