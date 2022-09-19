dcdf18880b0ab288c45160097f58a011183c046c
TheFungusAmongUs <nathan.pfeffer@protonmail.com>
2022-09-19 00:17:08 -0400

    Add Question class

--- cogs/question.py
+++ cogs/question.py
@@ -1,4 +1,29 @@
 import discord
+from typing import Optional, Type, Union
+
+
+class Question:
+
+    def __init__(self, title: str, body: str, author: Union[discord.Member, discord.User],
+                 closed: Optional[bool] = False):
+        self.title = title
+        self.body = body
+        self.author = author
+        self.closed = closed
+
+    def make_embed(self):
+        return discord.Embed(
+            title=self.title,
+            description=self.body,
+        ).add_field(name="Submitted By", value=f"<@!{self.author.id}>").set_thumbnail(url=self.author.avatar.url)
+
+    @classmethod
+    def from_json(cls, json_object: dict, bot: Type[discord.Bot]):
+        # noinspection PyArgumentList
+        return cls(**json_object.update({"author": bot.get_user(json_object["author"])}))
+
+    def to_json(self):
+        return self.__dict__.update({"author": self.author.id})
 
 
 class QuestionCog(discord.Cog):
