209b95860bd0dbb04cd6bcf22ffbf6e4d9dc2dde
TheFungusAmongUs <nathan.pfeffer@protonmail.com>
2022-09-18 23:26:10 -0400

    Fix bug "TypeError: 'str' object is not a mapping" in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from toml import load
 from utils.file_handling import *
 
 
@@ -6,8 +7,8 @@ def setup_toml() -> None:
     if Path('config/config.toml').exists():
         with open("config/config.toml", "r+") as file, open("config/base_config.toml", "r") as base_file:
             # Adds any extra fields from the base config file when updating the bot
-            base_toml = base_file.read()
-            current_toml = base_file.read()
+            base_toml = load(base_file)
+            current_toml = load(file)
             write_toml({**base_toml, **current_toml}, file)
 
     else:
