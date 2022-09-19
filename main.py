8e5f0cac84945555259c00b7550f7697eab22c6c
TheFungusAmongUs <nathan.pfeffer@protonmail.com>
2022-09-18 23:44:56 -0400

    Cogs will be used instead of adding commands in Bot subclass

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ import discord
 class GeoBot(discord.Bot):
     def __init__(self):
         super().__init__()
-        self.add_commands()
-
-    def add_commands(self):
-        pass
 
 
 def main():
