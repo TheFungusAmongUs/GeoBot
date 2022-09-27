e87221b00703b0416ed59a76eb72f71a843c0fa4
TheFungusAmongUs <nathan.pfeffer@protonmail.com>
2022-09-27 11:56:27 -0400

    Add feature for the bot to have a random activity

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import asyncio
 import discord
 import os
+import random
 import toml
-
+from discord.ext import tasks
 with open("config/config.toml", "r") as config:
     GLOBAL_CONFIG = toml.load(config)
+
+maps = [
+    "A Diverse World", "A Community World", "AI Generated World", "A Balanced Canada",
+    "A Diverse Sometimes Pinpointable AI Generated Tuas", "Plonk It", "The Daily Challenge",
+    "An Arbitrary United States", "A Balanced AI Generated India", "An Extra-Rural Mongolia", "A quiz",
+    "A Stochastic Populated Southern Cone", "A Balanced Australia", "An Extraordinary World",
+    "A Diverse Complete World", "Phuket Island or Chang Mai"
+]
 
 
 class GeoBot(discord.Bot):
@@ -14,6 +24,12 @@ class GeoBot(discord.Bot):
 
     async def on_ready(self):
         print(f"Bot is ready! Version info: {discord.__version__}")
+        await asyncio.sleep(5)
+        self.change_map.start()
+
+    @tasks.loop(hours=2)
+    async def change_map(self):
+        await self.change_presence(activity=discord.Game(name=random.choice(maps)))
 
 
 def main():
