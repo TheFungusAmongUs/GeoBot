c8b0e0acc240b3ea8f884ec355f17db0ac66c7a1
Nathan Pfeffer <nathan.pfeffer@protonmail.com>
2022-10-15 23:18:25 -0400

    Optimize setup.py and remove an unnecessary util file.

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import json
 import toml
 from pathlib import Path
-from utils.file_handling import *
 
 
 def setup_toml() -> None:
@@ -10,11 +9,13 @@ def setup_toml() -> None:
             # Adds any extra fields from the base config file when updating the bot
             base_toml = toml.load(base_file)
             current_toml = toml.load(file)
-            write_toml({**base_toml, **current_toml}, file)
+            file.seek(0)
+            file.truncate()
+            toml.dump({**base_toml, **current_toml}, file)
 
     else:
         with open("config/config.toml", "w") as file, open("config/base_config.toml", "r") as base_file:
-            write_toml(toml.load(base_file), file)
+            toml.dump(toml.load(base_file), file)
 
 
 data_files = ["posts.json", "tickets.json"]
@@ -22,13 +23,11 @@ data_files = ["posts.json", "tickets.json"]
 
 def setup_data() -> None:
     for file in data_files:
-        try:
-            with open(f"data/{file}", "r+") as fp:
-                json.load(fp)
-        except FileNotFoundError:
-            with open(f"data/{file}", "w") as fp:
-                json.dump([], fp)
+        if not Path(f'data/{file}').exists():
+            with open(f"data/{file}", "w") as data_file, open(f"data/base_{file}", "r") as base_file:
+                json.dump(json.load(base_file), data_file)
 
 
 if __name__ == "__main__":
+    setup_data()
     setup_toml()
